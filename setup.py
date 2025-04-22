ffbe5b413f15375c46ee7d4f3c9479c9cedfa9c1
Ryan Galloway <ryan@rsg.io>
2025-04-22 07:06:14 -0700

    set min versions for envstack and distman

--- setup.py
+++ setup.py
@@ -56,7 +56,7 @@ setup(
         ],
     },
     install_requires=[
-        "envstack==0.8.1",
+        "envstack>=0.8.1",
         "jsmin==3.0.1",
         "psutil==5.9.3",
         "PyYAML==5.3.1",
